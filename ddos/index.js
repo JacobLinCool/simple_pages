af96acc1a88b92e7f268e848ecbe75f86ccf6633
JacobLinCool <jacoblin2011@gmail.com>
2020-03-19 22:02:09 +0800

    Update index.js

--- ddos/index.js
+++ ddos/index.js
@@ -1,4 +1,5 @@
 function atk(a, b, c) {
+    document.getElementById('result').innerHTML = "測試進行中";
     Attack(parseInt(a), parseInt(b), c, "ddos", {update: function(now){document.getElementById('result').innerHTML = "已完成： " + now}}).then(s => {document.getElementById('result').innerHTML = "完成全部共"+s.success+"次測試";});
 }
 async function Attack(blocks=10, attacks=10, target, type="ddos", func={update: function(){}}) {
